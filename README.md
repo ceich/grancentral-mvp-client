db1c6162cb2b3d732127f8b331e5fa691b59e8b0
Chris Eich <ceich62@gmail.com>
2018-03-07 17:42:06 -0800

    Gave up on federated login. Users have to create username and password in the pool.

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This web app uses [AWS Amplify](https://github.com/aws/aws-amplify), with identity provided by [Facebook Login](https://developers.facebook.com/docs/facebook-login), to access the AWS Mobile Hub project [grancentral-mvp](https://console.aws.amazon.com/mobilehub/home?region=us-east-2#/4eb8b899-bc2c-44b5-b8b7-f925fd26e269/build).
 
-As the first and broadest user of the MH project's APIs, this repository also stores the [API code](awsmobilejs/backend/cloud-api). In order to keep the backend in sync, be sure to use [`awsmobile`](https://github.com/aws/awsmobile-cli) to pull before editing, and push after editing / testing.
-
 ## React reference
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Amplify from 'aws-amplify';
-import {
-  Authenticator,
-  FederatedSignIn,
-  SignIn,
-  ConfirmSignIn,
-  RequireNewPassword,
-  SignUp,
-  ConfirmSignUp,
-  VerifyContact,
-  ForgotPassword
- } from 'aws-amplify-react';
+import { withAuthenticator } from 'aws-amplify-react';
 
 import aws_exports from './aws-exports';
 import heart from './heart.svg';
@@ -18,74 +8,37 @@ import './App.css';
 
 Amplify.configure(aws_exports);
 
-const logger = new Amplify.Logger('App');
+// const hostedSignin = 'https://grancentral-ai-mvp.auth.us-east-2.amazoncognito.com/login?response_type=code&client_id=2gmaeoe97rv6o61006g3mp1hqu&redirect_uri=https://app.grancentral.ai/signin';
+
+// const logger = new Amplify.Logger('App');
 // Amplify.Logger.LOG_LEVEL = 'DEBUG';
 // logger.LOG_LEVEL = 'DEBUG';
-
-// Hide everything but Greetings in the Authenticator
-const AuthenticatorProps = {
-  hide: [
-    SignIn,
-    ConfirmSignIn,
-    RequireNewPassword,
-    SignUp,
-    ConfirmSignUp,
-    VerifyContact,
-    ForgotPassword
-  ],
-}
 
 // See https://developers.google.com/identity/sign-in/ios/start-integrating and
 // https://developers.google.com/identity/sign-in/android/start when time to do
 // Google sign-in for mobile apps.
-const FederatedProps = {
-  federated: {
-    amazon_client_id: 'amzn1.application-oa2-client.1b3a84b5d8f3430fa25244de70d08ab8',
-    facebook_app_id: '325521941271540',
-    google_client_id: '1062276029031-mu7ksht2e4e98ofg5o4r2tc6ohhtj6jt.apps.googleusercontent.com'
-  }
-}
+// const FederatedProps = {
+//   federated: {
+//     amazon_client_id: 'amzn1.application-oa2-client.1b3a84b5d8f3430fa25244de70d08ab8',
+//     facebook_app_id: '325521941271540',
+//     google_client_id: '1062276029031-mu7ksht2e4e98ofg5o4r2tc6ohhtj6jt.apps.googleusercontent.com'
+//   }
+// }
 
-class Body extends Component {
-  render() {
-    logger.debug('Body.render');
-    if (this.props.authState === 'signedIn') {
-      const user = this.props.authData;
-      return (
-        <div className="App-intro">
-          {user ?
-            ['id', 'email', 'name'].map((a) =>
-              <div key={a}>{[a, user[a]].join(': ')}</div>
-            ) :
-            'Loading'}
-        </div>
-      );
-    } else {
-      return null;
-      (
-        <p className="App-intro">
-          Please sign in to GranCentral!
-        </p>
-      );
-    }
-  }
-}
+const Body = ({authData}) => (
+  <div className="App-intro">
+    {authData.username || authData.name}
+  </div>
+)
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img className="App-logo" src={heart} alt="heart" />
-          <h1 className="App-title">GranCentral</h1>
-        </header>
-        <Authenticator {...AuthenticatorProps}>
-          <FederatedSignIn {...FederatedProps} />
-          <Body />
-        </Authenticator>
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <div className="App">
+    <header className="App-header">
+      <img className="App-logo" src={heart} alt="heart" />
+      <h1 className="App-title">GranCentral</h1>
+    </header>
+    <Body {...props}/>
+  </div>
+)
 
-export default App;
+export default withAuthenticator(App, true);
