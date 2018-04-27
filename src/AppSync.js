68b4ab2e22f89213fdc1bcf572303bac06185754
Chris Eich <ceich62@gmail.com>
2018-04-27 16:44:58 -0700

    Add magic-string files for ease of dev.

---
+++ src/AppSync.js
@@ -0,0 +1,5 @@
+export default {
+	"graphqlEndpoint": "https://gvu3cpw3lrf2xfenp65mtekg2m.appsync-api.us-east-2.amazonaws.com/graphql",
+	"region": "us-east-2",
+	"authenticationType": "AMAZON_COGNITO_USER_POOLS",
+}
